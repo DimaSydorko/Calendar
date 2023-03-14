c5cd35f87b8d77cc6f6afe9e269eb53c902e6573
DmytroSydorko <sydorko23@gmail@com>
2023-03-14 17:55:54 +0200

    Git deploy

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CssBaseline } from '@mui/material'
 import { SnackbarProvider } from 'notistack'
+import { BrowserRouter } from 'react-router-dom'
 
 import TasksProvider from 'State/useTasks'
 import SearchProvider from 'State/useSearch'
@@ -12,16 +13,18 @@ import 'index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <CalendarProvider>
-    <TasksProvider>
-      <SearchProvider>
-        <CssBaseline />
-        <SnackbarProvider maxSnack={3}>
-          <App />
-        </SnackbarProvider>
-      </SearchProvider>
-    </TasksProvider>
-  </CalendarProvider>
+  <BrowserRouter>
+    <CalendarProvider>
+      <TasksProvider>
+        <SearchProvider>
+          <CssBaseline />
+          <SnackbarProvider maxSnack={3}>
+            <App />
+          </SnackbarProvider>
+        </SearchProvider>
+      </TasksProvider>
+    </CalendarProvider>
+  </BrowserRouter>
 )
 
 reportWebVitals()
